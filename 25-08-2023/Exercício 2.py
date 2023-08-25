04dad11f2f7f294a7cf228b155a30b3b77541321
Chyri <chyricpmb@gmail.com>
2023-08-25 20:53:50 -0300

    Add files via upload

---
+++ 25-08-2023/Exercício 2.py
@@ -0,0 +1,16 @@
+valor = float(input('Digite um valor: '))
+
+if (valor == 1):
+    print('O valor é laranja.')
+
+elif (valor == 2):
+    print('O valor é verde.')
+
+elif (valor == 3):
+    print('O valor é amarelo.')
+
+elif (valor == 4):
+    print('O valor é preto.')
+
+else:
+    print('Erro! Seu valor não tem cor.')
