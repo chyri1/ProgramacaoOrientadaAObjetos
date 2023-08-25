578911804c88f4d6a5ab7f5dbd65119e4117edcd
Chyri <chyricpmb@gmail.com>
2023-08-25 20:54:07 -0300

    Add files via upload

---
+++ 25-08-2023/Exercício 3.py
@@ -0,0 +1,16 @@
+# 1 x 1 = 1
+# 1 x 2 = 2
+# 1 x 3 = 3
+# 1 x 4 = 4
+# 1 x 5 = 5
+# 1 x 6 = 6
+# 1 x 7 = 7
+# 1 x 8 = 8
+# 1 x 9 = 9
+# 1 x 10 = 10
+
+tab = int(input('Digite o valor da tabuada: '))
+print('\n')
+
+for i in range(1,11):
+    print(f'{tab} x {i} = {tab*i} ')
