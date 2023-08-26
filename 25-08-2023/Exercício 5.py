cd5e508555914f469ebbcb5d955b23404942572c
Chyri <chyricpmb@gmail.com>
2023-08-25 22:14:59 -0300

    Update Exercício 5.py

--- 25-08-2023/Exercício 5.py
+++ 25-08-2023/Exercício 5.py
@@ -1,10 +1,10 @@
 class Carro:
 
-    def __init__(self):
-        self.marca = ''
-        self.modelo = ''
-        self.cor = ''
-        self.velmax = ''
+    def __init__(self, m1, pera, c1, banana):
+        self.marca = pera
+        self.modelo = m1
+        self.cor = c1
+        self.velmax = banana
         self.comCombustível = True
 
     def acelerar():
@@ -16,9 +16,17 @@ class Carro:
     def zerandoCombustível(self):
         self.comCombustível = False
 
-carro1 = Carro()
-car = Carro()
-gol = Carro()
-ferrari = Carro()
+carro1 = Carro('Fiat', 'Uno', 'Preto', 100)
+# car = Carro()
+# gol = Carro()
+# ferrari = Carro()
 
-print(f' O modelo do carro é {carro1.modelo}')
+# carro1.modelo = 'Uno'
+# carro1.marca = 'Fiat'
+# carro1.copr = 'Preto'
+# carro1.velmax = 100
+
+print(f' O modelo do carro é {carro1.velmax}')
+# ferrari.acelerar()
+# gol.acelerar()
+# carro1.acelerar()
