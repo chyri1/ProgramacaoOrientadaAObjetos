31eb46f8b6bc396b4e438f2ecf04c318bf9bcc79
Chyri <chyricpmb@gmail.com>
2023-09-15 22:35:16 -0300

    Create main.py

---
+++ 15-09-2023/Escola/main.py
@@ -0,0 +1,9 @@
+from classEstudante import Estudante
+
+e1 = Estudante('00100276', 7.5, 'Arthur de Miranda Souza', 22, 'Sorocaba')
+
+print (f'Nome:  {e1.nome}')
+print (f'Idade: {e1.idade}')
+print (f'Endereço:  {e1.endereço}')
+print (f'Matrícula: {e1.matrícula}')
+print (f'Nota:  {e1.nota}')
